9149626299788ec9bc81865f264a09cb14fe6740
Nicholas Nicos Stamatakis <116750704+nick-stamatakis@users.noreply.github.com>
2023-12-03 13:54:23 -0500

    added post request

--- HelloWorldFunction/src/main/java/helloworld/GetQuestion.java
+++ HelloWorldFunction/src/main/java/helloworld/GetQuestion.java
@@ -16,6 +16,41 @@ public class GetQuestion implements RequestHandler<APIGatewayV2HTTPEvent, APIGat
 
     private static final Logger log = LoggerFactory.getLogger(GetQuestion.class);
 
+    private Map<String, String> parseJsonBody(String jsonBody) {
+        JSONObject jsonObject = new JSONObject(jsonBody);
+        Map<String, String> resultMap = new HashMap<>();
+
+        for (String key : jsonObject.keySet()) {
+            resultMap.put(key, jsonObject.getString(key));
+        }
+
+        return resultMap;
+    }
+
+
+    private APIGatewayV2HTTPResponse createSuccessResponse(String message) {
+        JSONObject responseBody = new JSONObject();
+        responseBody.put("message", message);
+
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .withHeaders(Collections.singletonMap("Content-Type", "application/json"))
+                .withBody(responseBody.toString())
+                .build();
+    }
+
+    private APIGatewayV2HTTPResponse createErrorResponse(String errorMessage) {
+        JSONObject responseBody = new JSONObject();
+        responseBody.put("error", errorMessage);
+
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(500)
+                .withHeaders(Collections.singletonMap("Content-Type", "application/json"))
+                .withBody(responseBody.toString())
+                .build();
+    }
+
+
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
         try {
@@ -36,6 +71,14 @@ public class GetQuestion implements RequestHandler<APIGatewayV2HTTPEvent, APIGat
         if ("GET".equals(input.getRequestContext().getHttp().getMethod())) {
             return getQuestion();
         }
+        else if ("POST".equals(input.getRequestContext().getHttp().getMethod())) {
+            // Retrieve data from the HTTP request
+            Map<String, String> body = parseJsonBody(input.getBody());
+
+            // Call the postQuestion method with the extracted values
+            return postQuestion(body);
+        }
+
 //        if (path != null && path.contains("api/get/question")) {
 //            return getQuestion();
 //        }
@@ -107,4 +150,53 @@ public class GetQuestion implements RequestHandler<APIGatewayV2HTTPEvent, APIGat
                 .withBody(jsonArray.toString())
                 .build();
     }
+
+    private APIGatewayV2HTTPResponse postQuestion(Map<String, String> body) {
+        log.info("Entering postQuestion");
+
+        try {
+            // Load the JDBC driver
+            Class.forName("com.amazonaws.secretsmanager.sql.AWSSecretsManagerPostgreSQLDriver").newInstance();
+
+            // Retrieve the connection info from the secret
+            final String URL = "dvexam-rwuser";
+            Properties info = new Properties();
+            info.put("user", "dvexam-rwuser");
+
+            try (Connection cxn = DriverManager.getConnection(URL, info)) {
+                // Create a PreparedStatement to insert a new question
+                String insertQuery = "INSERT INTO public.questions ("
+                        + "id, correctanswerchoice, difficulty, questiontext, "
+                        + "choiceatext, choicebtext, choicectext, choicedtext, choiceetext, "
+                        + "questiontype, section, answerexplanation, "
+                        + "categoriesalgebra, categoriesgeometry, imagelink, "
+                        + "equations, correctanswertext, imagesolutionlink"
+                        + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+                try (PreparedStatement pst = cxn.prepareStatement(insertQuery)) {
+                    // Set parameters for the PreparedStatement using values from the Map
+                    int i = 1;
+                    for (String key : body.keySet()) {
+                        pst.setString(i++, body.get(key));
+                    }
+
+                    // Execute the update
+                    int rowsAffected = pst.executeUpdate();
+
+                    if (rowsAffected > 0) {
+                        log.info("Question added successfully");
+                        return createSuccessResponse("Question added successfully");
+                    } else {
+                        log.error("Failed to add question");
+                        return createErrorResponse("Failed to add question");
+                    }
+                }
+            }
+
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            log.error("An unexpected error occurred: ", e);
+            return createErrorResponse("Internal Server Error");
+        }
+    }
+
 }
