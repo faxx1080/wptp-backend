6e553c26ca17a6489d45dab614fec0bc75e13d8d
Nicholas Nicos Stamatakis <116750704+nick-stamatakis@users.noreply.github.com>
2023-12-17 21:17:43 -0500

    fixed error with yaml

--- HelloWorldFunction/src/main/java/helloworld/controller/Router.java
+++ HelloWorldFunction/src/main/java/helloworld/controller/Router.java
@@ -43,12 +43,10 @@ public class Router {
                         "questionNumber", questionNumber
                 ));
             }
-            //Returns some data about the exam
+            //Returns some data about the examgit
             else if (url.startsWith("api/examattempts/")) {
                 Matcher matcher = generateMatcher(url, "^api/examattempts/(\\d+)$");
-                var userIds = matcher.group(1);
-                int userId = Integer.parseInt(userIds.trim());
-                var examAttemptIds = matcher.group(2);
+                var examAttemptIds = matcher.group(1);
                 int examAttemptId = Integer.parseInt(examAttemptIds.trim());
 
                 return new ExamAttemptData().handle(Map.of(
